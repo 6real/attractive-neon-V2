53d8a85d42a11dac75888a1153ec8568547f9f3d
Cyril Salvi <cyril.salvi8310@gmail.com>
2022-04-01 12:56:40 +0200

    Fix icone (menu + account ) in WP BO

--- web/app/themes/sage/app/primary-menu.php
+++ web/app/themes/sage/app/primary-menu.php
@@ -44,7 +44,7 @@ class Walker_Primary extends Walker_Nav_Menu
             $output .= '<a href="' . $permalink . '" class="-m-3 px-3 py-2 flex items-center rounded-lg">';
 
             if (!empty($icon)) {
-                $output .= $icon;
+                $output .= '<img class="h-6 w-6" src="'.$icon['url'].'" alt="">' ;
             } else {
                 $output .= '<svg class="flex-shrink-0 h-6 w-6 text-primary-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
--- web/app/themes/sage/resources/views/woocommerce/myaccount/navigation.blade.php
+++ web/app/themes/sage/resources/views/woocommerce/myaccount/navigation.blade.php
@@ -15,51 +15,57 @@
  * @version 2.6.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+  exit;
 }
 
-do_action( 'woocommerce_before_account_navigation' );
+do_action('woocommerce_before_account_navigation');
 ?>
 
 <section class="flex">
 
-<div class="w-1/4 mt-8">
-  <!-- Secondary sidebar -->
-  <nav aria-label="Sections" class="hidden flex-shrink-0 w-full bg-white border-r border-blue-gray-200 xl:flex xl:flex-col">
-    <div class="flex-shrink-0 h-16 px-6 border-b border-blue-gray-200 flex items-center">
-      <p class="text-lg font-medium text-blue-gray-900">{{_e('Account','woocommerce')}}</p>
-    </div>
-    <div class="flex-1 min-h-0 overflow-y-auto">
-
-      <?php foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
-
-      <a href="<?php echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?>" class="hover:bg-blue-50  hover:bg-opacity-50 flex p-6 border-b border-blue-gray-200">
-        <!-- Heroicon name: outline/bell -->
-        <svg class="flex-shrink-0 -mt-0.5 h-6 w-6 text-blue-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-        </svg>
-        <div class="ml-3 text-sm">
-          <p class="font-medium text-blue-gray-900"><?php echo esc_html( $label ); ?></p>
-<!--          <p class="mt-1 text-blue-gray-500">Enim, nullam mi vel et libero urna lectus enim. Et sed in maecenas tellus.</p>-->
-        </div>
-      </a>
-      <?php endforeach; ?>
-
-    </div>
-  </nav>
-
-</div>
+  <div class="w-1/4 mt-8">
+    <!-- Secondary sidebar -->
+    <nav aria-label="Sections"
+         class="hidden flex-shrink-0 w-full bg-white border-r border-blue-gray-200 xl:flex xl:flex-col">
+      <div class="flex-shrink-0 h-16 px-6 border-b border-blue-gray-200 flex items-center">
+        <p class="text-lg font-medium text-blue-gray-900">{{_e('Account','woocommerce')}}</p>
+      </div>
+      <div class="flex-1 min-h-0 overflow-y-auto">
+        @php $icons = get_field('menu-icons', 'option') @endphp
+        <?php foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
+        @foreach($icons['icones'] as $icon)
+          @if($icon['slug'] == $endpoint)
+            @php $image = $icon['icone']['url']; @endphp
+          @endif
+        @endforeach
 
 
-<!--<nav class="woocommerce-MyAccount-navigation">-->
-<!--	<ul>-->
-<!--		--><?php //foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
+        <a href="<?php echo esc_url(wc_get_account_endpoint_url($endpoint)); ?>"
+           class=" flex items-center hover:bg-blue-50 hover:bg-opacity-50 flex p-4 border-b border-blue-gray-200">
+
+          <img class="w-12" src="{{$image}}" alt="{{ esc_html($label)}}">
+          <div class="ml-3 text-sm">
+            <p class="font-medium text-sm text-blue-gray-900"><?php echo esc_html($label); ?></p>
+            <!--          <p class="mt-1 text-blue-gray-500">Enim, nullam mi vel et libero urna lectus enim. Et sed in maecenas tellus.</p>-->
+          </div>
+        </a>
+        <?php endforeach; ?>
+
+      </div>
+    </nav>
+
+  </div>
+
+
+  <!--<nav class="woocommerce-MyAccount-navigation">-->
+  <!--	<ul>-->
+  <!--		--><?php //foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
 <!--			<li class="--><?php //echo wc_get_account_menu_item_classes( $endpoint ); ?><!--">-->
-<!--				<a href="--><?php //echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?><!--">--><?php //echo esc_html( $label ); ?><!--</a>-->
-<!--			</li>-->
-<!--		--><?php //endforeach; ?>
+  <!--				<a href="--><?php //echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?><!--">--><?php //echo esc_html( $label ); ?><!--</a>-->
+  <!--			</li>-->
+  <!--		--><?php //endforeach; ?>
 <!--	</ul>-->
-<!--</nav>-->
+  <!--</nav>-->
 
-<?php do_action( 'woocommerce_after_account_navigation' ); ?>
+<?php do_action('woocommerce_after_account_navigation'); ?>
