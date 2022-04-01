93e5614a1d39893e0decd728b601256f2437c77d
Cyril Salvi <cyril.salvi8310@gmail.com>
2022-04-01 23:25:34 +0200

    Update related.blade.php

--- web/app/themes/sage/resources/views/woocommerce/single-product/related.blade.php
+++ web/app/themes/sage/resources/views/woocommerce/single-product/related.blade.php
@@ -1,52 +1,48 @@
-<?php
-/**
- * Related Products
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/single-product/related.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see         https://docs.woocommerce.com/document/template-structure/
- * @package     WooCommerce\Templates
- * @version     3.9.0
- */
+@php
+  /**
+   * Related Products
+   *
+   * This template can be overridden by copying it to yourtheme/woocommerce/single-product/related.php.
+   *
+   * HOWEVER, on occasion WooCommerce will need to update template files and you
+   * (the theme developer) will need to copy the new files to your theme to
+   * maintain compatibility. We try to do this as little as possible, but it does
+   * happen. When this occurs the version of the template file will be bumped and
+   * the readme will list any important changes.
+   *
+   * @see         https://docs.woocommerce.com/document/template-structure/
+   * @package     WooCommerce\Templates
+   * @version     3.9.0
+   */
 
-if (!defined('ABSPATH')) {
-  exit;
-}
+  if (!defined('ABSPATH')) {
+    exit;
+  }
 
-if ( $related_products ) : ?>
+  if ( $related_products ) :
+
+@endphp
 <section class="related products">
-  <?php
-  $heading = apply_filters('woocommerce_product_related_products_heading', __('Related products', 'woocommerce'));
+  @php
+    $heading = apply_filters('woocommerce_product_related_products_heading', __('Related products', 'woocommerce'));
+  @endphp
+  @if ( $heading )
+    <h2> {{esc_html($heading)}}</h2>
+  @endif
+  <div class="w-full border-l border-t border-gray-600 grid grid-cols-2 sm:mx-0 md:grid-cols-3 lg:grid-cols-4">
 
-  if ( $heading ) :
-  ?>
-  <h2><?php echo esc_html($heading); ?></h2>
-  <?php endif; ?>
+    @foreach ($related_products as $related_product)
 
-<!--		--><?php //woocommerce_product_loop_start(); ?>
-  <div class="-mx-px mx-auto border-l border-t border-gray-600 grid grid-cols-2 sm:mx-0 md:grid-cols-3 lg:grid-cols-4">
+      @php
+        $post_object = get_post($related_product->get_id());
 
+        setup_postdata($GLOBALS['post'] =& $post_object); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
 
-    <?php foreach ($related_products as $related_product) : ?>
+        wc_get_template_part('content', 'product');
+      @endphp
 
-					<?php
-      $post_object = get_post($related_product->get_id());
-
-      setup_postdata($GLOBALS['post'] =& $post_object); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
-
-      wc_get_template_part('content', 'product');
-      ?>
-
-			<?php endforeach; ?>
+    @endforeach
   </div>
-
-  <!--		--><?php //woocommerce_product_loop_end(); ?>
 
 </section>
 
