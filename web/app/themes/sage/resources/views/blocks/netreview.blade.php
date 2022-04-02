0b339a0291d656887ff0b71d8b7fb45cab3db2c4
Cyril Salvi <cyril.salvi8310@gmail.com>
2022-04-02 05:45:12 +0200

    Create netreview.blade.php

---
+++ web/app/themes/sage/resources/views/blocks/netreview.blade.php
@@ -0,0 +1,18 @@
+{{--
+  Title: NetReview Attraxtive
+  Description: Avis vérifiés block
+  Category: attractive
+  Icon: grid-view
+  Keywords: banner
+  Mode: auto
+  PostTypes: page post
+  SupportsMode: true
+  SupportsMultiple: true
+  --}}
+
+
+<section class="{{ $block['classes'] }}">
+  <iframe id='AV_widget_iframe' frameBorder="0" width="100%" height="100%"
+          src="//cl.avis-verifies.com/fr/cache/d/2/d/d2d16cb1-9564-7b14-2dfe-77d442100050/widget4/d2d16cb1-9564-7b14-2dfe-77d442100050horizontal_index.html"></iframe>
+
+</section>
