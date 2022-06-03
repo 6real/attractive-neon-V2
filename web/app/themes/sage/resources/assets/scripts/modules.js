4973bcfe545885727b363bd3ac3a7ff387ad3ef8
Cyril Salvi <cyril.salvi8310@gmail.com>
2022-06-03 10:23:15 +0200

    Fix Newsletter Style & JS

--- web/app/themes/sage/resources/assets/scripts/modules.js
+++ web/app/themes/sage/resources/assets/scripts/modules.js
@@ -1,6 +1,6 @@
 export {default as header} from './modules/header';
 //TODO FINIR LA NEWSLETTER
-// export {default as newsletter} from './modules/newsletter';
+export {default as newsletter} from './modules/newsletter';
 export {default as woocommerceGallery} from './modules/woocommerceGallery';
 export {default as notice} from './modules/notices';
 export {default as productCategories} from './modules/product-categories';
